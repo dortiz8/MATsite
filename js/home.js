e05e0bf022f5bda319ed9b13c7bf8f78143d721f
dortiz8 <53200770+dortiz8@users.noreply.github.com>
2020-10-16 13:35:14 -0400

    general info div add

--- js/home.js
+++ js/home.js
@@ -92,3 +92,33 @@ calendarButton.onclick = ()=>{
    
    
 }
+
+//GENERAL CLOSE BUTTON
+
+let xButtons = document.getElementsByClassName("x"); 
+
+for (const x of xButtons) {
+    x.onclick = ()=>{
+        x.parentNode.parentNode.style.display = 'none';  
+    }
+}
+
+//GENERAL INFO LINKS 
+
+let links = document.querySelectorAll("#generalInfoSection a"); 
+
+for(const link of links){
+    link.onclick = ()=>{
+        if (link.innerText === "Privacy Policy") {
+            document.getElementsByClassName("generalInformation")[0].style.display = "block"; 
+    
+        }
+        else if(link.innerText === "National D Lincese Details"){
+
+            window.open('https://learning.ussoccer.com/coach/courses/available/3/course-info', '_blank');
+        }
+        else if(link.innerText === "Waiver Information"){
+            document.getElementsByClassName("generalInformation")[1].style.display = "block";
+        }
+    }
+}
